6818c64844681757f70b29ba6dc456a4e3359b0d
MAlejandroR <manuelromeromiguel@gmail.com>
2024-02-15 14:30:46 +0100

    proyecto de clase del certificado

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,11 @@
+<header class="h-15v bg-header flex flex-row
+ justify-between p-5 items-center">
+    <img class="h-2/3" src="{{asset("images/logo.png")}}" alt="logo">
+    <h1 class="text-white uppercase text-6xl">Gestión Laravel</h1>
+    <div class="space-x-4">
+        <button class = "text-white btn glass">Login</button>
+        <button  class = "btn glass text-white">Register</button>
+    </div>
+
+
+</header>
---
+++ resources/views/components/nav.blade.php
@@ -0,0 +1,13 @@
+<nav class ="hidden lg:flex h-10v bg-nav flex flex-row justify-start items-center px-5 space-x-5">
+    <a href="about" class=" btn btn-primary"> About</a>
+    <a href="" class="  btn btn-info"> Contacta</a>
+    <a href="" class="  btn btn-secondary"> Proyectos</a>
+    <a href="main" class="  btn btn-neutral"> Principal</a>
+</nav>
+<nav class ="flex lg:hidden bg-nav flex flex-col justify-start items-center px-5 space-y-5">
+    <a href="about" class=" btn btn-primary"> About</a>
+    <a href="" class="  btn btn-info"> Contacta</a>
+    <a href="" class="  btn btn-secondary"> Proyectos</a>
+    <a href="main" class="  btn btn-neutral"> Principal</a>
+</nav>
+
